2c2dbbb30928bf920ed295cb92fc36e0f523b578
Lucas Melo <luluco250@gmail.com>
2019-06-01 11:42:19 -0300

    Moves and status.

--- creature/_base.py
+++ creature/_base.py
@@ -16,14 +16,26 @@ class Base:
 	):
 		self.index = index
 		self.name = name
+		self.type1 = type1
+		self.type2 = type2
+
+		# Base stats.
 		self.attack = attack
 		self.defense = defense
 		self.sp_attack = sp_attack
 		self.sp_defense = sp_defense
 		self.speed = speed
 		self.health = health
-		self.type1 = type1
-		self.type2 = type2
+		
+		# Combat variables.
+		self.curr_health = health
+
+		# Buffs reset after battle.
+		self.attack_buff = 0
+		self.defense_buff = 0
+		self.sp_attack_buff = 0
+		self.sp_defense_buff = 0
+		self.speed_buff = 0
 	
 	def status(self):
 		type2_text = ""
@@ -31,4 +43,23 @@ class Base:
 		if self.type2 != Typing.Null:
 			type2_text = self.type2.name
 		
-		print("[{}]".format())
+		print(
+"""[{}]
+HP: {}/{}
+ATK: {}
+DEF: {}
+SP_ATK: {}
+SP_DEF: {}
+SPD: {}""".format(
+		self.name,
+		self.curr_health,
+		self.health,
+		self.attack,
+		self.defense,
+		self.sp_attack,
+		self.sp_defense,
+		self.speed
+	))
+	
+	def do_move(self, other, move):
+		move(self, other)
--- main.py
+++ main.py
@@ -8,3 +8,13 @@ lucas = Trainer("Lucas", 20, [
 	creature.Bulbasaur()
 ])
 lucas.list_creatures()
+
+def tackle(a, b):
+	b.curr_health -= round(40 * (a.attack / 255))
+
+pikachu = lucas.creatures[0]
+charmander = lucas.creatures[1]
+pikachu.do_move(charmander, tackle)
+
+pikachu.status()
+charmander.status()
